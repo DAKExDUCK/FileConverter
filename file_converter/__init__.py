913daf0fdc34ffe42b61afbf38e064417d9fa199
Arsen Gabdulin <arsengabdulin228@gmail.com>
2023-06-07 13:38:24 +0600

    add file_converter, jpg and png

---
+++ file_converter/__init__.py
@@ -0,0 +1,23 @@
+from file_converter.jpg import JPG, JPGs
+from file_converter.png import PNG, PNGs
+from file_converter.pdf import PDF
+
+
+__all__ = [
+    'JPG',
+    'JPGs',
+    'PNG',
+    'PNGs',
+    'PDF',
+]
+
+
+def define_class_for_format(key:str):
+    if key.upper() not in __all__:
+        return None
+    
+    index = __all__.index(key.upper())
+    name = __all__[index]
+    obj = globals().get(name)
+    if isinstance(obj, type):
+        return obj
