1574f51a7ad6267c7b7dab81a62957d77e7dca7b
Arsen Gabdulin <arsengabdulin228@gmail.com>
2023-07-04 18:56:32 +0600

    add setup

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='file_converter',
+      version='0.1',
+      description='Simple way to convert files to another formats',
+      packages=['file_converter'],
+      author_email='arsengadbulin228@gmail.com',
+      zip_safe=False)
